947d4c46d313d94ead34b4ecf477737723830a67
Aswin-2808 <your.email@example.com>
2023-07-19 16:05:36 +0530

    Success

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("It is awesome!");
+    System.out.println(2*5+20-15/3);
+int a=5,b=5,c=a+b;
+double db=a;
+    System.out.println("Calculated Answer: " + c);
+    System.out.println(db); 
+  }
+}
+
+public class Main{
+public static void myMethod(){
+	System.out.println("hi aswin"+(20+30));
+}
+public static void main(String[] args){
+myMethod();
+}
+}*/
+
+
+public class Main {
+    public static void myMethod(int age) {
+        if (age < 24) {
+            System.out.println("Not Eligible");
+        } else {
+            System.out.println("Eligible");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMethod(25);
+    }
+}
